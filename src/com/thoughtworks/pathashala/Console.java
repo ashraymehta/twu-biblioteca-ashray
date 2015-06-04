8aee8d6b105a3b9630504c8207e8fbb118c493e3
Ashray Mehta <ashrayme@INashrayme.local>
2015-06-04 22:04:28 +0530

    Added a spec to test display of list of books

--- src/com/thoughtworks/pathashala/Console.java
+++ src/com/thoughtworks/pathashala/Console.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.pathashala;
 
+import java.util.List;
+
 // Handles all I/O to and from console
 public class Console {
 
     public void printWelcomeMessage() {
         System.out.println("Welcome");
+    }
+
+    public void printListOfBooks(List<String> bookList) {
+        for(String book : bookList) {
+            System.out.println(book);
+        }
     }
 }
--- test/com/thoughtworks/pathashala/ConsoleTest.java
+++ test/com/thoughtworks/pathashala/ConsoleTest.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.pathashala;
 
+import com.thoughtworks.pathashala.helper.BookDataReader;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,6 +29,24 @@ public class ConsoleTest {
         assertThat(actualOutput, is("Welcome" + System.lineSeparator()));
     }
 
+    @Test
+    public void testPrintListOfBooks() throws Exception {
+        String projectRoot = System.getProperty("user.dir");
+        String filePath = projectRoot + File.separator + "BookList.txt";
+        BookDataReader bookDataReader = new BookDataReader(filePath);
+        Library library = new Library();
+        library.initialize(bookDataReader.getListOfBooks());
+        Console console = new Console();
+        console.printListOfBooks(library.getAvailableBooks());
+
+        String actualOutput = outputStream.toString();
+        String expectedOutput = "Introduction to Programming" + System.lineSeparator() +
+                "The Pragmatic Programmer" + System.lineSeparator() +
+                "Harry Potter and the Philosopher's Stone" + System.lineSeparator();
+
+        assertThat(actualOutput, is(expectedOutput));
+    }
+    
     @After
     public void tearDown() throws Exception {
         System.setOut(null);
