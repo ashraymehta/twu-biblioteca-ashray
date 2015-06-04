a768c359dd382530a553d679c0398b02c4c4e6ed
Ashray Mehta <ashrayme@INashrayme.local>
2015-06-04 20:25:39 +0530

    Refactored BookDataReader class; extracted a method that reads from file

--- src/com/thoughtworks/pathashala/helper/BookDataReader.java
+++ src/com/thoughtworks/pathashala/helper/BookDataReader.java
@@ -14,22 +14,24 @@ public class BookDataReader {
     public ArrayList<String> getListOfBooks() {
         ArrayList<String> listOfBooks = new ArrayList<>();
         File file = new File(filePath);
-        if(file.exists()) {
-            try {
-                FileReader fileReader = new FileReader(file);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-                String readLine;
-
-                while((readLine = bufferedReader.readLine()) != null) {
-                    listOfBooks.add(readLine.trim());
-                }
-            } catch (FileNotFoundException e) {
-                return listOfBooks;
-            } catch (IOException e) {
-                return listOfBooks;
-            }
+        if (file.exists()) {
+            readBookDataFromFile(listOfBooks, file);
 
         }
         return listOfBooks;
+    }
+
+    private void readBookDataFromFile(ArrayList<String> listOfBooks, File file) {
+        try {
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String readLine;
+
+            while ((readLine = bufferedReader.readLine()) != null) {
+                listOfBooks.add(readLine.trim());
+            }
+        } catch (IOException e) {
+            return;
+        }
     }
 }
