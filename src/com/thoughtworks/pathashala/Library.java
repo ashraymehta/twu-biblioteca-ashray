b0a9219697d38b8ef3ef9160c7866ebddaf568a3
Ashray Mehta <ashrayme@INashrayme.local>
2015-06-04 21:35:59 +0530

    Added a spec to test initialization of list of books in library

--- src/com/thoughtworks/pathashala/Library.java
+++ src/com/thoughtworks/pathashala/Library.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.pathashala;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Library {
+    private ArrayList<String> availableBooks;
+
+    public Library() {
+        availableBooks = new ArrayList<>();
+    }
+
+    public void initialize(ArrayList<String> listOfBooksRead) {
+        availableBooks.clear();
+        if (listOfBooksRead != null)
+            availableBooks.addAll(listOfBooksRead);
+    }
+
+    public List<String> getAvailableBooks() {
+        return Collections.unmodifiableList(availableBooks);
+    }
 }
--- test/com/thoughtworks/pathashala/LibraryTest.java
+++ test/com/thoughtworks/pathashala/LibraryTest.java
@@ -1,4 +1,29 @@
 package com.thoughtworks.pathashala;
 
+import com.thoughtworks.pathashala.helper.BookDataReader;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
 public class LibraryTest {
+
+    @Test
+    public void testLibraryHasBooksAvailableAfterInitialization() throws Exception {
+        String projectRoot = System.getProperty("user.dir");
+        String filePath = projectRoot + File.separator + "BookList.txt";
+        BookDataReader bookDataReader = new BookDataReader(filePath);
+        ArrayList<String> listOfBooksRead = bookDataReader.getListOfBooks();
+        Library library = new Library();
+        library.initialize(listOfBooksRead);
+
+        assertThat(library.getAvailableBooks(), is(notNullValue()));
+        assertThat(library.getAvailableBooks(), is(not(empty())));
+    }
 }
