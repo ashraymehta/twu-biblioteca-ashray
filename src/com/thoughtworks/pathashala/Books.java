03279f75249202a6b29b0e42b62f93808b850ecf
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-09 08:42:02 +0530

    Added functionality of Books class to initialize itself

--- src/com/thoughtworks/pathashala/Books.java
+++ src/com/thoughtworks/pathashala/Books.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.pathashala;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Books {
     private List<Book> listOfBooks;
+
+    public Books() {
+        listOfBooks = new ArrayList<>();
+        listOfBooks.add(new Book("Harry Potter and the Philosopher's Stone", "JK Rowling", 1997));
+        listOfBooks.add(new Book("Harry Potter and the Chamber of Secrets", "JK Rowling", 1998));
+    }
 
     public Books(List<Book> bookArrayList) {
         this.listOfBooks = bookArrayList;
