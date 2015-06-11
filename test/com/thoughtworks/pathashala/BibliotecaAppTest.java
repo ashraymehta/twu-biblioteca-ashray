1be21146354273478f3437db20251d0a222c17d6
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-11 20:18:58 +0530

    Added spec to check main menu is printed upon startup

--- test/com/thoughtworks/pathashala/BibliotecaAppTest.java
+++ test/com/thoughtworks/pathashala/BibliotecaAppTest.java
@@ -8,10 +8,12 @@ import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -44,5 +46,23 @@ public class BibliotecaAppTest {
         bibliotecaApp.start();
 
         Mockito.verify(consoleOut).printWelcomeMessage();
+    }
+
+    @Test
+    public void shouldPrintMainMenu() throws Exception {
+        String command = "2";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(command.getBytes());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Scanner scanner = new Scanner(byteArrayInputStream);
+        Menu menu = new Menu(menuItemsMappedToMenuAction, menuItemsMappedToSerials);
+        MenuView menuView = new MenuView(menu, scanner, new PrintStream(outputStream));
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(consoleOut, menuView);
+        bibliotecaApp.start();
+
+        String actualOutput = outputStream.toString();
+        String expectedOutput = "1. List books" + System.lineSeparator() +
+                "2. Quit" + System.lineSeparator() + System.lineSeparator();
+
+        assertEquals(expectedOutput, actualOutput);
     }
 }
