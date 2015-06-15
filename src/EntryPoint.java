f0ed38ce89fde782045c46384f9a8441735374d9
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-15 09:03:05 +0530

    Refactored: Moved entry point to a separate class EntryPoint

---
+++ src/EntryPoint.java
@@ -0,0 +1,56 @@
+import com.twu.*;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> listOfAvailableBooks = getAvailableBooks();
+        Books availableBooks = new Books(listOfAvailableBooks);
+        PrintStream consoleOutStream = new PrintStream(System.out);
+        ArrayList<Book> listOfCheckedOutBooks = new ArrayList<>();
+        Books checkedOutBooks = new Books(listOfCheckedOutBooks);
+        BooksView booksView = new BooksView(availableBooks, consoleOutStream);
+        ReturnBookView returnBookView = new ReturnBookView(checkedOutBooks, scanner, consoleOutStream);
+        CheckoutBookView checkoutBookView = new CheckoutBookView(availableBooks, scanner, consoleOutStream);
+        Library library = new Library(listOfAvailableBooks, listOfCheckedOutBooks);
+        CheckoutBookAction checkoutBookAction = new CheckoutBookAction(checkoutBookView, library);
+
+        HashMap<Integer, String> menuItemsMappedToSerials = new HashMap<>();
+        HashMap<Integer, MenuAction> menuItemsMappedToMenuAction = new HashMap<>();
+        populateHashMaps(booksView, returnBookView, library, checkoutBookAction, menuItemsMappedToSerials, menuItemsMappedToMenuAction);
+
+        Menu menu = new Menu(menuItemsMappedToMenuAction, menuItemsMappedToSerials);
+        MenuView menuView = new MenuView(menu, scanner, consoleOutStream);
+        ConsoleOut consoleOut = new ConsoleOut(consoleOutStream);
+
+        BibliotecaApp bibliotecaApp = new BibliotecaApp(consoleOut, menuView);
+        bibliotecaApp.start();
+    }
+
+    private static void populateHashMaps(BooksView booksView, ReturnBookView returnBookView,
+                                         Library library, CheckoutBookAction checkoutBookAction,
+                                         HashMap<Integer, String> menuItemsMappedToSerials,
+                                         HashMap<Integer, MenuAction> menuItemsMappedToMenuAction) {
+        menuItemsMappedToSerials.put(1, "List books");
+        menuItemsMappedToMenuAction.put(1, new ListBooksAction(booksView));
+        menuItemsMappedToSerials.put(2, "Checkout Book");
+        menuItemsMappedToMenuAction.put(2, checkoutBookAction);
+        menuItemsMappedToSerials.put(3, "Return Book");
+        MenuAction returnBookAction = new ReturnBookAction(returnBookView, library);
+        menuItemsMappedToMenuAction.put(3, returnBookAction);
+        menuItemsMappedToSerials.put(4, "Quit");
+        menuItemsMappedToMenuAction.put(4, new QuitAction());
+    }
+
+    private static ArrayList<Book> getAvailableBooks() {
+        ArrayList<Book> listOfAvailableBooks = new ArrayList<>();
+        listOfAvailableBooks.add(new Book("Harry Potter and the Philosopher's Stone", "JK Rowling", 1997));
+        listOfAvailableBooks.add(new Book("Harry Potter and the Chamber of Secrets", "JK Rowling", 1998));
+        return listOfAvailableBooks;
+    }
+}
--- src/com/twu/BibliotecaApp.java
+++ src/com/twu/BibliotecaApp.java
@@ -1,10 +1,5 @@
 package com.twu;
 
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
-
 public class BibliotecaApp {
 
     private ConsoleOut consoleOut;
@@ -15,42 +10,7 @@ public class BibliotecaApp {
         this.menuView = menuView;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Book> listOfAvailableBooks = new ArrayList<>();
-        listOfAvailableBooks.add(new Book("Harry Potter and the Philosopher's Stone", "JK Rowling", 1997));
-        listOfAvailableBooks.add(new Book("Harry Potter and the Chamber of Secrets", "JK Rowling", 1998));
-        PrintStream consoleOutStream = new PrintStream(System.out);
-        Books availableBooks = new Books(listOfAvailableBooks);
-        ArrayList<Book> listOfCheckedOutBooks = new ArrayList<>();
-        Books checkedOutBooks = new Books(listOfCheckedOutBooks);
-        BooksView booksView = new BooksView(availableBooks, consoleOutStream);
-        ReturnBookView returnBookView = new ReturnBookView(checkedOutBooks, scanner, consoleOutStream);
-        CheckoutBookView checkoutBookView = new CheckoutBookView(availableBooks, scanner, consoleOutStream);
-        Library library = new Library(listOfAvailableBooks, listOfCheckedOutBooks);
-        CheckoutBookAction checkoutBookAction = new CheckoutBookAction(checkoutBookView, library);
-
-        HashMap<Integer, String> menuItemsMappedToSerials = new HashMap<>();
-        HashMap<Integer, MenuAction> menuItemsMappedToMenuAction = new HashMap<>();
-        menuItemsMappedToSerials.put(1, "List books");
-        menuItemsMappedToMenuAction.put(1, new ListBooksAction(booksView));
-        menuItemsMappedToSerials.put(2, "Checkout Book");
-        menuItemsMappedToMenuAction.put(2, checkoutBookAction);
-        menuItemsMappedToSerials.put(3, "Return Book");
-        MenuAction returnBookAction = new ReturnBookAction(returnBookView, library);
-        menuItemsMappedToMenuAction.put(3, returnBookAction);
-        menuItemsMappedToSerials.put(4, "Quit");
-        menuItemsMappedToMenuAction.put(4, new QuitAction());
-
-        Menu menu = new Menu(menuItemsMappedToMenuAction, menuItemsMappedToSerials);
-        MenuView menuView = new MenuView(menu, scanner, consoleOutStream);
-        ConsoleOut consoleOut = new ConsoleOut(consoleOutStream);
-
-        BibliotecaApp bibliotecaApp = new BibliotecaApp(consoleOut, menuView);
-        bibliotecaApp.start();
-    }
-
-    void start() {
+    public void start() {
         consoleOut.printWelcomeMessage();
         MenuAction actionTaken = null;
         while (!(actionTaken instanceof QuitAction)) {
