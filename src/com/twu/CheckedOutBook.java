5e60c86bf53bef6127e763d67d99280e6db8ee42
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-15 15:56:28 +0530

    Added CheckedOutBook and a string representation of it

---
+++ src/com/twu/CheckedOutBook.java
@@ -0,0 +1,7 @@
+package com.twu;
+
+public class CheckedOutBook extends BookAbstract {
+    public CheckedOutBook(String title, String author, int yearPublished, int id) {
+        super(title, author, yearPublished, id);
+    }
+}
