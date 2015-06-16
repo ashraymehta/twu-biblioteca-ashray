74c7e2daeab84a10255c2a76879ada337734bdc7
Ashray Mehta <ashraymehta@users.noreply.github.com>
2015-06-16 08:52:52 +0530

    Added ability for Searcher to search in a list of books

--- src/com/twu/Searcher.java
+++ src/com/twu/Searcher.java
@@ -1,4 +1,26 @@
 package com.twu;
 
+import com.twu.book.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Searcher {
+
+    private List<Book> books;
+
+    public Searcher(List<Book> books) {
+        this.books = books;
+    }
+
+    public Book search(String title) {
+        List<Book> matchingBooks = new ArrayList<>();
+        for (Book book : books) {
+            book.addToListIfTitleMatches(matchingBooks, title);
+        }
+        if (matchingBooks.size() > 0)
+            return matchingBooks.get(0);
+        else
+            return null;
+    }
 }
--- test/com/twu/SearcherTest.java
+++ test/com/twu/SearcherTest.java
@@ -1,7 +1,27 @@
 package com.twu;
 
-import static org.junit.Assert.*;
+import com.twu.book.AvailableBook;
+import com.twu.book.Book;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
 
 public class SearcherTest {
+    @Test
+    public void shouldSearchForTitleInListOfBooks() throws Exception {
+        List<Book> books = new ArrayList<>();
+        AvailableBook bookOne = new AvailableBook("Title 1", "Author 1", 1000, 1);
+        AvailableBook bookTwo = new AvailableBook("Title 2", "Author 2", 500, 2);
+        books.add(bookOne);
+        books.add(bookTwo);
+        Searcher searcher = new Searcher(books);
 
+        Book actualResult = searcher.search("Title 1");
+        Book expectedResult = bookOne;
+
+        assertEquals(expectedResult, actualResult);
+    }
 }
